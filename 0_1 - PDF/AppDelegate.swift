d2db6f9f46f06afae6de294a0d228143e9c43afc
justinmreina <justinmreina@gmail.com>
2017-11-09 16:21:37 -0800

    (+)    initial commit

---
+++ 0_1 - PDF/AppDelegate.swift
@@ -0,0 +1,56 @@
+/************************************************************************************************************************************/
+/** @file		AppDelegate.swift
+ *  @project    0_1 - PDF
+ * 	@brief		x
+ * 	@details	x
+ *
+ * 	@author		Justin Reina, Firmware Engineer, Jaostech
+ * 	@created	11/12/15
+ * 	@last rev	x
+ *
+ *
+ * 	@notes		x
+ *
+ * 	@section	Opens
+ * 			none current
+ *
+ * 	@section	Legal Disclaimer
+ * 			All contents of this source file and/or any other Jaostech related source files are the explicit property on Jaostech
+ * 			Corporation. Do not distribute. Do not copy.
+ */
+/************************************************************************************************************************************/
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?;
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow.init(frame: UIScreen.main.bounds);
+        
+        self.window?.backgroundColor = UIColor.white;
+        
+        let viewController:ViewController = ViewController();
+        
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false;
+        
+        self.window?.rootViewController = viewController;
+        
+        self.window?.makeKeyAndVisible();
+        
+        print("AppDelegate.application():          Application launch complete");
+        
+        return true;
+    }
+
+    func applicationWillResignActive(_ application: UIApplication)    { return; }
+    func applicationDidEnterBackground(_ application: UIApplication)  { return; }
+    func applicationWillEnterForeground(_ application: UIApplication) { return; }
+    func applicationDidBecomeActive(_ application: UIApplication)     { return; }
+    func applicationWillTerminate(_ application: UIApplication)       { return; }
+}
+
